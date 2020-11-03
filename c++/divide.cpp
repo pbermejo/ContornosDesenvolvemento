b1f0d066cb625197b1f34639441c49e267f221a7
pbermejo <pbermejo.dev@gmail.com>
2020-11-03 19:47:45 +0100

    engade código en C++ e documentación

---
+++ c++/divide.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+
+using namespace std;
+
+int main(){
+	int a, b;
+	cout << "Introduce el primer numero entero: ";
+	cin >> a;
+	cout << "Introduce el segundo numero entero: ";
+	cin >> b;
+	cout << "El resultado de la division es " << (a / b);
+	return 0;
+}
